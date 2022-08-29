45fb83d94a5933089bd5729450d09a0ec53d0c60
ChaitanyaKongara <chaitanyakongara96@gmail.com>
2022-08-29 12:46:24 +0530

    Added binary search on file

--- index.py
+++ index.py
@@ -1,7 +1,30 @@
 import sys, os, shutil, subprocess, bz2, re, xml.sax, nltk, Stemmer
 from nltk.corpus import stopwords
 
-STOP_WORDS = set(stopwords.words("english")).union({"reflist", "refbegin", "ref", "citation", "refend", "references", "category", "infobox", "external", "links", "https", "http", "www", "org", "com", "edu", "jpg", "jpeg", "pdf", "png"})
+STOP_WORDS = set(stopwords.words("english")).union(
+    {
+        "reflist",
+        "refbegin",
+        "ref",
+        "citation",
+        "refend",
+        "references",
+        "category",
+        "infobox",
+        "external",
+        "links",
+        "https",
+        "http",
+        "www",
+        "org",
+        "com",
+        "edu",
+        "jpg",
+        "jpeg",
+        "pdf",
+        "png",
+    }
+)
 inline_references = ""
 pattern = {
     "i": "\{\{infobox(.+\n)+\}\}",
@@ -9,14 +32,14 @@ pattern = {
     "r": "== ?references ?==\n(.+\n)+\n",
     "l": "== ?external links ?==\n(.+\n)+\n",
     "rs": "== ?references ?==",
-    "ls": "== ?external links ?=="
+    "ls": "== ?external links ?==",
 }
 page_count = 1
 word_count = 1
 index_cnt = 0
 total_tokens_dump = 0
 total_tokens_index = 0
-index_word_size_division = 1 
+index_word_size_division = 1
 temp_dir = "XtempX/"
 # subprocess.check_call([sys.executable, "-m", "pip", "install", "sortedcontainers"])
 from sortedcontainers import SortedDict, SortedList
@@ -24,10 +47,23 @@ from sortedcontainers import SortedDict, SortedList
 inverted_index = SortedDict()
 title_file = open("title_map", "w")
 title_id = []
+
+
 def text_processing(text):
-    tokens = re.sub(r"\W|_", " ", text.encode("ascii", "ignore").decode()) # remove non alphanumeric
-    tokens = [ token for token in tokens.split() if token not in STOP_WORDS and len(token) > 2 and not token[0].isnumeric() and not token[1].isnumeric() ]
+    tokens = re.sub(
+        r"\W|_", " ", text.encode("ascii", "ignore").decode()
+    )  # remove non alphanumeric
+    tokens = [
+        token
+        for token in tokens.split()
+        if token not in STOP_WORDS
+        and len(token) > 2
+        and not token[0].isnumeric()
+        and not token[1].isnumeric()
+        and not token.isnumeric()
+    ]
     return Stemmer.Stemmer("english").stemWords(tokens)
+
 
 def extract_field(field, text):
     global pattern, inline_references
@@ -46,6 +82,7 @@ def extract_inline_references(text):
         inline_references = " ".join([inline_references, reference.group()])
     return re.sub(r"<ref.*>.*</ref>", " ", text)
 
+
 def get_first_index(field, text):
     match_idx = len(text)
     matches = re.finditer(rf"{pattern[field]}", text)
@@ -54,30 +91,38 @@ def get_first_index(field, text):
         break
     return match_idx
 
+
 # refs -> infobox -> Categories -> references -> external links -> body
 def extract_fields(text):
     page_field = {}
-    # text = extract_inline_references(text)
+    text = extract_inline_references(text)
     ref_index = get_first_index("rs", text)
     links_index = get_first_index("ls", text)
-    infobox_text = text[:min(ref_index, links_index)]
-    rest_text = text[min(ref_index, links_index):]
+    infobox_text = text[: min(ref_index, links_index)]
+    rest_text = text[min(ref_index, links_index) :]
     page_field["i"], infobox_text = extract_field("i", infobox_text)
-    page_field["b"] = text_processing(infobox_text)
+    page_field["b"] = text_processing(
+        re.sub("\{.*?\}|\[.*?\]|\=\=.*?\=\=", "", infobox_text)
+    )
     page_field["c"], rest_text = extract_field("c", rest_text)
     if ref_index != len(text) and links_index != len(text):
-        rest_text_ref = rest_text[:abs(ref_index - links_index)]
-        rest_text_links = rest_text[abs(ref_index - links_index):]
+        rest_text_ref = rest_text[: abs(ref_index - links_index)]
+        rest_text_links = rest_text[abs(ref_index - links_index) :]
         page_field["r"], rest_text_ref = extract_field("r", rest_text_ref)
         page_field["l"], rest_text_links = extract_field("l", rest_text_links)
-        page_field["b"] += text_processing(rest_text_ref) + text_processing(rest_text_links)
+        page_field["b"] += text_processing(
+            re.sub("\{.*?\}|\[.*?\]|\=\=.*?\=\=", "", rest_text_ref)
+        ) + text_processing(re.sub("\{.*?\}|\[.*?\]|\=\=.*?\=\=", "", rest_text_links))
     else:
         if ref_index != len(text):
             page_field["r"], rest_text = extract_field("r", rest_text)
         elif links_index != len(text):
-            page_field["l"], rest_text = extract_field("l", rest_text)       
-        page_field["b"] += text_processing(rest_text)
+            page_field["l"], rest_text = extract_field("l", rest_text)
+        page_field["b"] += text_processing(
+            re.sub("\{.*?\}|\[.*?\]|\=\=.*?\=\=", "", rest_text)
+        )
     return page_field
+
 
 def update_inverted_index(page_field):
     global inverted_index
@@ -86,16 +131,13 @@ def update_inverted_index(page_field):
             continue
         for word in page_field[field]:
             if not inverted_index.__contains__(word):
-                inverted_index[word] = {
-                    page_field["id"]: {
-                        field: 0
-                    }
-                }
+                inverted_index[word] = {page_field["id"]: {field: 0}}
             elif page_field["id"] not in inverted_index[word]:
                 inverted_index[word][page_field["id"]] = {field: 0}
             if field not in inverted_index[word][page_field["id"]]:
                 inverted_index[word][page_field["id"]][field] = 0
             inverted_index[word][page_field["id"]][field] += 1
+
 
 def write_inverted_index_to_file(index_cnt):
     global inverted_index
@@ -104,22 +146,24 @@ def write_inverted_index_to_file(index_cnt):
     for word, posting_list in inverted_index.items():
         line = "".join([word, "\n"])
         for doc_id, table in posting_list.items():
-            if line[-1] == '\n':
+            if line[-1] == "\n":
                 line = "".join([line, str(doc_id)])
             else:
                 line = ";".join([line, str(doc_id)])
             for field, freq in table.items():
                 if freq:
                     line = " ".join([line, "".join([field, str(freq)])])
-        content.append("".join([line,'\n']))
+        content.append("".join([line, "\n"]))
     os.makedirs(temp_dir, exist_ok=True)
     with open(file_name, "w") as file:
         if len(content):
             file.writelines(content)
 
+
 def close_files(fds):
     for fd in fds:
         fd.close()
+
 
 def merge_indices(index_cnt):
     print("index_cnt", index_cnt)
@@ -128,7 +172,7 @@ def merge_indices(index_cnt):
     pq = SortedList()
     open_temp_files = []
     for idx in range(index_cnt):
-        open_temp_files.append(open("".join([file_name, str(idx)]),"r"))
+        open_temp_files.append(open("".join([file_name, str(idx)]), "r"))
         word = open_temp_files[idx].readline()[:-1]
         if len(word):
             pq.add((word, idx))
@@ -147,7 +191,9 @@ def merge_indices(index_cnt):
             lines_cnt = 0
             index_file_name = word[:index_word_size_division]
             index_file = open("".join([sys.argv[2], f"/{index_file_name}"]), "w")
-        line = "\n".join([top[0][index_word_size_division:], open_temp_files[top[1]].readline()[:-1]])
+        line = ":".join(
+            [top[0][index_word_size_division:], open_temp_files[top[1]].readline()[:-1]]
+        )
         while len(pq) and pq[0][0] == word:
             top_ = pq.pop(0)
             line = ";".join([line, open_temp_files[top_[1]].readline()[:-1]])
@@ -156,7 +202,7 @@ def merge_indices(index_cnt):
                 pq.add((new_word, top_[1]))
         total_tokens_index += 1
         lines_cnt += 1
-        lines.append("".join([line,"\n"]))
+        lines.append("".join([line, "\n"]))
         if lines_cnt % 10000:
             index_file.writelines(lines)
             lines.clear()
@@ -169,6 +215,7 @@ def merge_indices(index_cnt):
         index_file.writelines(lines)
         lines.clear()
     index_file.close()
+
 
 class WikiArticleHandler(xml.sax.ContentHandler):
     def startElement(self, tag, attributes):
@@ -193,13 +240,17 @@ class WikiArticleHandler(xml.sax.ContentHandler):
     def endElement(self, tag):
         global inverted_index, index_cnt, inline_references, total_tokens_dump, page_count, word_count, pattern
         if tag == "page":
-            title_id.append("".join(["".join([str(self.id),"\n"]), "".join([self.title.strip(), "\n"])]))
+            title_id.append(
+                "".join(
+                    ["".join([str(self.id), ":"]), "".join([self.title.strip(), "\n"])]
+                )
+            )
             page_field = extract_fields(self.text)
             page_field["t"] = text_processing(self.title)
             page_field["id"] = self.id
             self.id = None
             update_inverted_index(page_field)
-            if page_count % 50000 == 0:
+            if page_count % 100000 == 0:
                 title_file.writelines(title_id)
                 title_id.clear()
                 write_inverted_index_to_file(index_cnt)
@@ -207,16 +258,18 @@ class WikiArticleHandler(xml.sax.ContentHandler):
                 inverted_index.clear()
             page_count += 1
         if tag == "title":
-            total_tokens_dump += len(tuple(re.finditer(r"\s",self.title)))
+            total_tokens_dump += len(tuple(re.finditer(r"\s", self.title)))
         if tag == "text":
-            total_tokens_dump += len(tuple(re.finditer(r"\s",self.text)))
+            total_tokens_dump += len(tuple(re.finditer(r"\s", self.text)))
+
 
 def main():
     if len(sys.argv) < 4:
         sys.exit("ERR: Too few arguments")
     global index_cnt, inverted_index
-    wiki_dump = bz2.open(sys.argv[1])
-    xml.sax.parse(wiki_dump, WikiArticleHandler())
+    # wiki_dump = bz2.open(sys.argv[1])
+    # xml.sax.parse(wiki_dump, WikiArticleHandler())
+    xml.sax.parse(open(sys.argv[1], "r"), WikiArticleHandler())
     title_file.writelines(title_id)
     title_id.clear()
     write_inverted_index_to_file(index_cnt)
@@ -224,10 +277,19 @@ def main():
     inverted_index.clear()
     print("wc:", word_count)
     print("pc:", page_count)
-    merge_indices(index_cnt=9)
+    merge_indices(index_cnt)
     title_file.close()
     file = open(f"{sys.argv[3]}", "w")
-    file.writelines("".join(["".join([str(total_tokens_dump),"\n"]), "".join([str(total_tokens_index), "\n"])]))
+    file.writelines(
+        "".join(
+            [
+                "".join([str(total_tokens_dump), "\n"]),
+                "".join([str(total_tokens_index), "\n"]),
+            ]
+        )
+    )
     file.close()
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()
--- search.py
+++ search.py
@@ -1,15 +1,69 @@
 import sys, os, re, nltk, Stemmer
 from nltk.corpus import stopwords
 
-STOP_WORDS = set(stopwords.words("english")).union({"reflist", "refbegin", "ref", "citation", "refend", "references", "category", "infobox", "external", "links", "https", "http", "www", "org", "com", "edu", "jpg", "jpeg", "pdf", "png"})
+STOP_WORDS = set(stopwords.words("english")).union(
+    {
+        "reflist",
+        "refbegin",
+        "ref",
+        "citation",
+        "refend",
+        "references",
+        "category",
+        "infobox",
+        "external",
+        "links",
+        "https",
+        "http",
+        "www",
+        "org",
+        "com",
+        "edu",
+        "jpg",
+        "jpeg",
+        "pdf",
+        "png",
+    }
+)
 INDEX_DIR = sys.argv[1]
 QUERY = sys.argv[2]
+
+
+def get_value(key, filename):
+    low = 0
+    high = os.path.getsize(filename) - 1
+    with open(filename, "rb") as fd:
+        while True:
+            mid = (low + high) // 2
+            startIdx = mid
+            fd.seek(mid)
+            while startIdx >= 0 and (fd.read(1) != b"\n" or startIdx == mid):
+                fd.seek(-2, 1)
+                startIdx -= 1
+            startIdx += 1
+            if startIdx <= low:
+                break
+            line = fd.readline().decode().split(":", 1)
+            if line[0] == key:
+                return line[1]
+            elif line[0] > key:
+                high = mid - 1
+            else:
+                low = mid + 1
+        line = fd.readline().decode().split(":", 1)
+        if line[0] == key:
+            return line[1]
+        else:
+            return ""
 
 
 def main():
     global QUERY
     if len(sys.argv) < 3:
         sys.exit("ERR: Too few arguments")
-    split_query(QUERY)
-if __name__ == '__main__':
+    # split_query(QUERY)
+    print(get_value("15841678", "title_map"))
+
+
+if __name__ == "__main__":
     main()
